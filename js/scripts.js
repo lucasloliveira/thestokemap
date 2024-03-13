d79e96433070aedfd242dff0e3621e8d28939fbf
Lucas <lucaslimaoliv@gmail.com>
2024-03-13 11:51:00 -0400

    Update counter

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(window).load(function() {
 
 $(document).ready(function(){
   $("#counter").countdown({
-  until: new Date(2016, 12 - 1, 15),
+  until: new Date(2024, 5, 15),
   format: 'dHMS'
   });
 
